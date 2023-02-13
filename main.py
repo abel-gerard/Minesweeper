b8a7f6f369a06fa2b8b46d2e26d9fea83f080e57
abel-gerard <67419571+abel-gerard@users.noreply.github.com>
2023-02-13 14:50:42 +0100

    Basic input

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from libs.game import*
 
-game = Game(*Game.DIFFICULTY["expert"])   
+def main():
+    difficulty = input("Choose the difficulty between 'easy', 'medium' and 'expert'. 'quit' if you want to quit the game. ")
+    if difficulty == 'quit': return
+    if difficulty not in ("easy", "medium", "expert"): 
+        main()
+        return
+
+    Game(*Game.DIFFICULTY[difficulty])   
+    
+if __name__ == "__main__":
+    main()
